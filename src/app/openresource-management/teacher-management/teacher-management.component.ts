82796ef7d97734d0e77c02f92db9b891b7bfcf1a
Marshall-Sun <910204019@qq.com>
2021-01-02 16:35:26 +0800

    fix TeacherManagement

--- src/app/openresource-management/teacher-management/teacher-management.component.ts
+++ src/app/openresource-management/teacher-management/teacher-management.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-import {CourseManagementBackHalfService} from '../../service/course-management-back-half/course-management-back-half.service';
-import {NzModalService, NzNotificationService} from 'ng-zorro-antd';
-import {Observable} from 'rxjs';
+import {NzMessageService, NzModalService, NzNotificationService} from 'ng-zorro-antd';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-teacher-management',
@@ -10,101 +9,79 @@ import {Observable} from 'rxjs';
   styleUrls: ['./teacher-management.component.less']
 })
 export class OpenresourceTeacherManagementComponent implements OnInit {
-
   teacherList = [];
+  listOfOption = [];
+  selectedValue = null;
   courseId: string;
   teachplanId:string;
-  loading: boolean = false;
-
 
   constructor(
-    private courseManagement$: CourseManagementBackHalfService,
-    private _notification: NzNotificationService,
-    private _modal: NzModalService
+    private _modal: NzModalService,
+    private msg: NzMessageService,
+    private http: HttpClient,
   ) { }
 
   ngOnInit() {
     this.courseId = location.pathname.split('/')[3];
-    this.teachplanId = location.pathname.split('/')[5];
-    this.getTeacherList(this.teachplanId);
+    this.getTeacherList();
+    this.getAvailableTeacherList();
   }
 
-
-  drop(event: CdkDragDrop<any[]>) {
+  async drop(event: CdkDragDrop<any[]>) {
     moveItemInArray(this.teacherList, event.previousIndex, event.currentIndex);
-    let idList = [];
-    this.teacherList.forEach(item => {
-      idList.push(item.id)
-    });
-    this.courseManagement$.sortTeacherSeq(this.teachplanId, idList).subscribe(result => {
-      this.getTeacherList(this.teachplanId);
-      this._notification.success(
-        '排序成功！',
-        ''
-      )
-    }, error1 => {
-      this._notification.error(
-        '排序失败！',
-        `${error1.error}`
-      )
-    })
+    try {
+      let etc = this.teacherList.reduce((str, teacher) => str + "&tableIdList=" + teacher.tableId, "");
+      await this.http.put(`/course/open/sortOpenCourseTeacher?${etc.slice(1)}`, {}).toPromise();
+      this.getTeacherList();
+      this.msg.success("排序成功");
+    } catch (e) {
+      this.msg.error("排序失败");
+    }
   }
 
-  getTeacherList(planId: string) {
-    this.courseManagement$.getTeacherList(this.teachplanId).subscribe(result => {
-      this.teacherList = result.data
-    }, error1 => {
-      this._notification.error(
-        '获取教师列表失败！',
-        `${error1.error}`
-      )
-    })
+  async getTeacherList() {
+    try {
+      let teacherList: any = await this.http.get(`/course/open/getOpenCourseTeacherList?openCourseId=${this.courseId}`).toPromise();
+      this.teacherList = teacherList;
+    } catch (e) {
+      this.msg.error("信息初始化失败");
+    }
   }
 
-  addTeacher(data: any) {
-    this.courseManagement$.addTeacherIntoCourse(this.courseId, this.teachplanId, data).subscribe(result => {
-      this.getTeacherList(this.teachplanId);
-      let i;
-      this.courseManagement$.addingStatus.subscribe(value => i = value);
-      this.courseManagement$.addingStatus.next(i + 1);
-      this._notification.success(
-        '添加教师成功！',
-        ''
-      )
-    }, error1 => {
-      this._notification.error(
-        '添加教师失败！',
-        `${error1.error}`
-      )
-    })
+  async getAvailableTeacherList() {
+    try {
+      let res: any = await this.http.get(`/course/open/getAvailableTeacher?openCourseId=${this.courseId}`).toPromise();
+      this.listOfOption = res.data.map((teacher: any) => ({
+        label: teacher.username,
+        value: teacher.userId,
+      }));
+    } catch (e) {
+      this.msg.error("可选教师信息初始化失败");
+    }
   }
 
-  onChange(data: any) {
-    this.teachplanId = data;
-    this.getTeacherList(this.teachplanId)
+  async addTeacher() {
+    try {
+      await this.http.post(`/course/open/addOpenCourseTeacher?openCourseId=${this.courseId}&teacherId=${this.selectedValue}`, {}).toPromise();
+      location.reload();
+      this.msg.success("添加成功");
+    } catch (e) {
+      this.msg.error("添加失败");
+    }
   }
 
-  close(id: string) {
+  delete(id: string) {
     this._modal.confirm({
       nzTitle: '是否删除该教师？',
-      nzOnOk: () => {
-        this.courseManagement$.deleteTeacher(this.teachplanId, id).subscribe(result => {
-          this.getTeacherList(this.teachplanId);
-          let i;
-          this.courseManagement$.addingStatus.subscribe(value => i = value);
-          this.courseManagement$.addingStatus.next(i + 1);
-          this._notification.success(
-            '删除成功！',
-            ''
-          )
-        }, error1 => {
-          this._notification.error(
-            '删除失败！',
-            `${error1.error}`
-          )
-        })
+      nzOnOk: async () => {
+        try {
+          await this.http.delete(`/course/open/deleteOpenCourseTeacher?tableId=${id}`).toPromise();
+          this.msg.success("删除成功");
+          location.reload();
+        } catch (e) {
+          this.msg.error("删除失败");
+        }
       }
     })
   }
-
 }
